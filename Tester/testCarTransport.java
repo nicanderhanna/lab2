8ebd0776e93e370cf415f70e5fd930e5baafb52a
nicanderhanna <hanna.nicander@hotmail.com>
2023-11-16 09:44:46 +0100

    skrivigt klart car transport och tester till hela den

--- Tester/testCarTransport.java
+++ Tester/testCarTransport.java
@@ -1,10 +1,81 @@
 import org.junit.Before;
+import org.junit.Test;
+
+import java.awt.*;
+
+import static org.junit.Assert.assertTrue;
 
 public class testCarTransport {
 
     private CarTransport carTransport;
 
+    private CarTransport carTransport2;
+    private Scania scania;
+
+    private Volvo240 volvo;
+
+    private Saab95 saab;
+
     @Before
-    public void test
+    public void init(){
+        carTransport = new CarTransport();
+        scania = new Scania();
+        volvo = new Volvo240();
+        saab = new Saab95();
+        carTransport2 = new CarTransport();
+    }
+
+
+    @Test
+    public void testTrailerUp(){
+        carTransport.startEngine();
+        carTransport.trailerDown();
+        assertTrue(carTransport.trailerUp == false);
+        carTransport.stopEngine();
+        carTransport.trailerUp();
+        assertTrue(carTransport.trailerUp == true);
+    }
+
+    @Test
+    public void testLoadCar(){
+        carTransport.loadCar(scania);
+        carTransport.loadCar(volvo);
+        carTransport.trailerUp();
+        carTransport.loadCar(saab);
+        assertTrue(carTransport.carsOnTruck[2] == null);
+        carTransport.trailerDown();
+        carTransport.loadCar(saab);
+        assertTrue(carTransport.carsOnTruck[0] == scania);
+        assertTrue(carTransport.carsOnTruck[2] == saab);
+
+
+    }
+    @Test
+    public void testUnloadCar(){
+        carTransport.loadCar(scania);
+        carTransport.loadCar(volvo);
+        carTransport.unloadCar(scania);
+        assertTrue(carTransport.carsOnTruck[0] == scania);
+        assertTrue(carTransport.carsOnTruck[1] == volvo);
+        carTransport.unloadCar(volvo);
+        assertTrue(carTransport.carsOnTruck[1] == null);
+    }
+
+    @Test
+    public void testLoadCarTransport(){
+        carTransport.loadCar(carTransport2);
+        assertTrue(carTransport.carsOnTruck[0]==null);
+    }
+
+    @Test
+    public void testMove(){
+        carTransport.loadCar(scania);
+        carTransport.startEngine();
+        carTransport.move();
+        assertTrue(carTransport.getPosY() == 0.1);
+        assertTrue(scania.getPosY() == 0.1);
+    }
 
 }
+
+
--- src/CarTransport.java
+++ src/CarTransport.java
@@ -1,34 +1,44 @@
 
 import java.awt.*;
-import java.util.Arrays;
 
 public class CarTransport extends Trucks {
 
     Boolean trailerUp = false;
 
-    Cars[] carsOnTruck = new Cars[2];
+    Cars[] carsOnTruck = new Cars[3];
 
-    public CarTransport(int nrDoors, double enginePower, Color color, String modelName) {
+    public CarTransport() {
         super(2, 100, Color.green, "Car Transport", "North");
     }
 
     public void trailerUp() {
-        boolean b = this.getCurrentSpeed() != 0;
-        if (b) {
+        //boolean b = this.getCurrentSpeed() != 0;
+        if (this.getCurrentSpeed() != 0) {
             System.out.println("Can't move trailer when Truck is in moving");
         } else {
             trailerUp = true;
         }
     }
 
-    public void trailedDown() {
-        boolean b = this.getCurrentSpeed() != 0;
-        if (b) {
+    public void trailerDown() {
+        //boolean b = this.getCurrentSpeed() != 0;
+        if (this.getCurrentSpeed() != 0) {
             System.out.println("Can't move trailer when Truck is in moving");
 
-        } else {
+        }
+        else {
             trailerUp = false;
         }
+    }
+
+    public int getNrOfCarsInTruck() {
+        int x = 0;
+        for (int i = 0; i < carsOnTruck.length; i++) {
+            if (carsOnTruck[i] != null) {
+                x= x+ 1;
+            }
+        }
+        return x;
     }
 
     public void loadCar(Cars car){
@@ -36,12 +46,16 @@ public class CarTransport extends Trucks {
             System.out.println("The truck can't carry another car transport");
         }
 
-        else if (carsOnTruck.length == 3 && trailerUp == false){
-            System.out.println("The truck is full or the trailer is up");
+        else if (getNrOfCarsInTruck() == 3){
+            System.out.println("The truck is full");
+        }
+
+        else if (trailerUp == true){
+            System.out.println("The truck is up");
         }
 
         else if (Math.abs(this.getPosY() - car.getPosY()) <1 && Math.abs(this.getPosX() - car.getPosX()) <1 ){
-            carsOnTruck[carsOnTruck.length] = car;
+            carsOnTruck[getNrOfCarsInTruck()] = car;
             car.setPosY(this.getPosY());
             car.setPosX(this.getPosX());
 
@@ -49,11 +63,10 @@ public class CarTransport extends Trucks {
     }
 
     private int checkIfCarIsInList(Cars car){
-        int x = 0;
-        for ( int i = 0 ; i< carsOnTruck.length; i++){
+        int x = 10;
+        for ( int i = 0 ; i < carsOnTruck.length; i++){
             if ( car == carsOnTruck[i])
             { x = i;}
-            else {x=10;}
         }
         return x;
     }
@@ -63,12 +76,13 @@ public class CarTransport extends Trucks {
         if (carInList == 10) {
             System.out.println("Car not on truck");
         }
-        else if (carInList == carsOnTruck.length -1){
-            carsOnTruck[carsOnTruck.length - 1] = null;
+        else if (carInList == getNrOfCarsInTruck() -1){
+            carsOnTruck[getNrOfCarsInTruck()-1] = null;
             car.setPosY(this.getPosY() + 0.1);
             car.setPosX(this.getPosX() + 0.1);
         }
     }
+
 
     @Override
     public void move(){
@@ -76,29 +90,28 @@ public class CarTransport extends Trucks {
         double posY = getPosY();
         double posX = getPosX();
         double currentSpeed = getCurrentSpeed();
-
             switch (direction) {
                 case "North":
                     setPosY(posY + currentSpeed);
-                    for (int i = 0; i< carsOnTruck.length; i++) {
+                    for (int i = 0; i< getNrOfCarsInTruck(); i++) {
                         carsOnTruck[i].setPosY(posY + currentSpeed);
                     }
                     break;
                 case "South":
                     setPosY( posY - currentSpeed);
-                    for (int i = 0; i< carsOnTruck.length; i++) {
+                    for (int i = 0; i< getNrOfCarsInTruck(); i++) {
                         carsOnTruck[i].setPosY(posY + currentSpeed);
                     }
                     break;
                 case "West":
                     setPosX( posX - currentSpeed);
-                    for (int i = 0; i< carsOnTruck.length; i++) {
+                    for (int i = 0; i< getNrOfCarsInTruck(); i++) {
                         carsOnTruck[i].setPosX(posX + currentSpeed);
                     }
                     break;
                 case "East":
                     setPosX( posX + currentSpeed);
-                    for (int i = 0; i< carsOnTruck.length; i++) {
+                    for (int i = 0; i< getNrOfCarsInTruck(); i++) {
                         carsOnTruck[i].setPosX(posX + currentSpeed);
                     }
                     break;
@@ -111,5 +124,5 @@ public class CarTransport extends Trucks {
 
 
 
-}
+
 
